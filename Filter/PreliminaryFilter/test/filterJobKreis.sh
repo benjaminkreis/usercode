6d4a502d00d1c9429343f496a375da2c1b27ac96
kreis <>
2010-01-07 01:53:19 +0000

    change configuration file name to run

--- Filter/PreliminaryFilter/test/filterJobKreis.sh
+++ Filter/PreliminaryFilter/test/filterJobKreis.sh
@@ -2,6 +2,6 @@
 cd /afs/cern.ch/user/k/kreis/scratch0/CMSSW_3_3_2/src/Filter/PreliminaryFilter/test
 eval `scramv1 runtime -sh`
 cd -
-cmsRun /afs/cern.ch/user/k/kreis/scratch0/CMSSW_3_3_2/src/Filter/PreliminaryFilter/test/filterPatEvents_LM3_cfg.py
+cmsRun /afs/cern.ch/user/k/kreis/scratch0/CMSSW_3_3_2/src/Filter/PreliminaryFilter/test/preliminaryFilter_cfg.py
 #rfcp lm3_filtered.root /castor/cern.ch/user/k/kreis/LM3Filtered/lm3_filtered.root
 
