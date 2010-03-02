0b42919d9593622bbbeb3f71cbf02bca1835d075
kreis <>
2010-03-02 20:38:41 +0000

    using trees

--- CUJEMtools/eventsByNbs/results/analyzeABCD.C
+++ CUJEMtools/eventsByNbs/results/analyzeABCD.C
@@ -20,30 +20,20 @@
 #include <iomanip>
 #include <assert.h>
 
+////////
+//B//C//
+////////
+//A//D//
+////////
+//C is signal region
+
 void analyzeABCD(){
   gROOT->SetStyle("Plain");
   gStyle->SetPalette(1);
   gStyle->SetOptStat("e");
   
-  TString histname = "H_minDPhi_MET";
-  
-  TFile* file1 = TFile::Open("plots_QCD_Pt170_nBJetsLTEQGTmerged.root","READ");
-  TH2F* hist1 = (TH2F*) gDirectory->Get(histname);
-  
-  TAxis* xax =  hist1->GetXaxis();
-  TAxis* yax =  hist1->GetYaxis();
-  cout << "x axis from " << xax->GetXmin() <<" to " <<  xax->GetXmax()<< endl;;
-  cout << "y axis from " << yax->GetXmin() << " to " << yax->GetXmax()<< endl;;
-
-  xax->SetTitle("MET (GeV)");
-  yax->SetTitle("min Delta Phi (MET, jet 1 2 3)");
-  hist1->Draw("COLZ");
-  gPad->SetRightMargin(.18);
-  gPad->Modified();
- 
   float pi=4*atan(1.0);
-  float nA=0, nB=0, nC=0, nD=0;
-  
+
   float borderv1a=0.0;
   float borderv1b=150.0;
   float borderv2a=150.0;
@@ -53,28 +43,28 @@ void analyzeABCD(){
   float borderh2a=0.3;
   float borderh2b=pi;
 
+   float nA=0, nB=0, nC=0, nD=0;
 
-  // HISTOGRAM for fit 
-  const Double_t yBins[4] = {borderh1a, borderh1b, borderh2a, borderh2b};
- 
-  int fitNum = 5;
-  float f_xWidth = (borderv1b-borderv1a)/fitNum;
-  Double_t f_xBins[fitNum+1];
-  for(int i = 0; i<=fitNum; i++){
-    f_xBins[i]=borderv1a+i*f_xWidth;
-    cout <<"histf " << f_xBins[i] << endl;
-  }
-  TH2F* histf = new TH2F("H_ratio_setup","Ratio Setup",fitNum, f_xBins, 3, yBins);
-  
-  // HISTOGRAM for extension
-  int extendedNum = 20;
-  float e_xWidth = (borderv2b-borderv2a)/extendedNum;
-  Double_t e_xBins[extendedNum+1];
-  for(int i = 0; i<=extendedNum; i++){
-    e_xBins[i]=borderv2a+i*e_xWidth;
-    cout << "histe " << e_xBins[i] << endl;
-  }
+   TCanvas * C_ABCD = new TCanvas("C_ABCD", "Canvas ABCD", 1000, 1000);
+   C_ABCD->Divide(2,2);
+   C_ABCD->cd(1);
+  /////////////////////////////
+  //        HISTOGRAMS       //
+  /////////////////////////////
 
+  //plot 2d histogram for visual purposes
+  TString histname = "H_minDPhi_MET";
+  TFile* file1 = TFile::Open("plots_LM0_nBJetsLT2.root","READ");
+  TH2F* hist1 = (TH2F*) gDirectory->Get(histname);
+  TAxis* xax =  hist1->GetXaxis();
+  TAxis* yax =  hist1->GetYaxis();
+  xax->SetTitle("MET (GeV)");
+  yax->SetTitle("min Delta Phi (MET, jet 1 2 3)");
+  hist1->Draw("COLZ");
+  gPad->SetRightMargin(.18);
+  gPad->Modified();
+  //cout << "x axis from " << xax->GetXmin() <<" to " <<  xax->GetXmax()<< endl;;
+  //cout << "y axis from " << yax->GetXmin() << " to " << yax->GetXmax()<< endl;;
   TLine* lineAb = new TLine(borderv1a,borderh1a,borderv1b, borderh1a);
   TLine* lineAt = new TLine(borderv1a,borderh1b,borderv1b, borderh1b);
   TLine* lineAl = new TLine(borderv1a,borderh1a,borderv1a, borderh1b);
@@ -139,61 +129,96 @@ void analyzeABCD(){
   lineDt->Draw();
   lineDl->Draw();
   lineDr->Draw();
-
-  int nBinsX =  hist1->GetNbinsX();
-  int nBinsY =  hist1->GetNbinsY();
   
-  for(int xbin = 1; xbin<= nBinsX; xbin++){
-   
-    for(int ybin = 1; ybin<= nBinsY; ybin++){
-      float xlow =  xax->GetBinLowEdge(xbin);
-      float xup =  xax->GetBinUpEdge(xbin); 
-      float ylow = yax->GetBinLowEdge(ybin);
-      float yup =  yax->GetBinUpEdge(ybin);
-      //cout << "xlow " << xlow << endl;
-      //cout << "ylow " << ylow << endl;
-
-      histf->Fill(xlow,ylow,hist1->GetBinContent(xbin,ybin));
-
-      if( (xlow>=borderv1a) && (xlow<borderv1b) && (ylow>=borderh1a) && (ylow<borderh1b) ){
-	nA+=hist1->GetBinContent(xbin,ybin);
-      }
-      else if( (xlow>=borderv2a) && (xlow<borderv2b) && (ylow>=borderh1a) && (ylow<borderh1b) ){
-	nD+=hist1->GetBinContent(xbin,ybin);
-      }
-      else if( (xlow>=borderv1a) && (xlow<borderv1b) && (ylow>=borderh2a) && (ylow<borderh2b) ){
-	nB+=hist1->GetBinContent(xbin,ybin);
-      }
-      else if( (xlow>=borderv2a) && (xlow<borderv2b) && (ylow>=borderh2a) && (ylow<borderh2b) ){
-	nC+=hist1->GetBinContent(xbin,ybin);
-      }
-      else { assert(0);}
-      
-    }//end loop over ybin
-  }//end loop over xbin
+  //prep for histograms A, B, C, D
+  const Double_t yBinsL[2] = {borderh1a, borderh1b};
+  const Double_t yBinsU[2] = {borderh2a, borderh2b};
   
-  //histf->Draw("COLZ");
+  int fitNum = 5; //number of bins in xL region, used to fit ratio
+  float xWidthL = (borderv1b-borderv1a)/fitNum;
+  Double_t xBinsL[fitNum+1];
+  for(int i = 0; i<=fitNum; i++){
+    xBinsL[i]=borderv1a+i*xWidthL;
+    cout <<"xBinsL: " << xBinsL[i] << endl;
+  }
   
+  int extendedNum = 20; //number of bins in xU region, used to calculate nC
+  float xWidthU = (borderv2b-borderv2a)/extendedNum;
+  Double_t xBinsU[extendedNum+1];
+  for(int i = 0; i<=extendedNum; i++){
+    xBinsU[i]=borderv2a+i*xWidthU;
+    cout << "xBinsU: " << xBinsU[i] << endl;
+  }
+  
+  // histogram for A, B, C, D
+  TH2F* histA = new TH2F("H_A", "Region A", fitNum, xBinsL, 1, yBinsL);
+  TH2F* histB = new TH2F("H_B", "Region B", fitNum, xBinsL, 1, yBinsU);
+  TH2F* histC = new TH2F("H_C", "Region C", extendedNum, xBinsU, 1, yBinsU);
+  TH2F* histD = new TH2F("H_D", "Region D", extendedNum, xBinsU, 1, yBinsL);
+
+  //////////////////
+  //LOOP over TREE//
+  //////////////////
+  TString treename = "T_minDPhi_MET";
+  TTree* tree1 = (TTree*) gDirectory->Get(treename);
+  int numEntries = tree1->GetEntries();
+  cout << "numEntries: " << numEntries << endl;
+  
+  float x = -1;
+  float y = -1;
+  tree1->SetBranchAddress("MET",&x);
+  tree1->SetBranchAddress("minDPhi",&y);
+  
+  for(int i = 1; i<= numEntries; i++){ //loop over tree
+    tree1->GetEvent(i);
+    
+    //COUNT nA, nB, nC, nD
+    if( (x>=borderv1a) && (x<borderv1b) && (y>=borderh1a) && (y<borderh1b) ){
+      nA++;
+    }
+    else if( (x>=borderv2a) && (x<borderv2b) && (y>=borderh1a) && (y<borderh1b) ){
+      nD++;
+    }
+    else if( (x>=borderv1a) && (x<borderv1b) && (y>=borderh2a) && (y<borderh2b) ){
+      nB++;
+    }
+    else if( (x>=borderv2a) && (x<borderv2b) && (y>=borderh2a) && (y<borderh2b) ){
+      nC++;
+    }
+    else { assert(0);}
+    
+    //Fill histogram A, B, C, D
+    histA->Fill(x,y);
+    histB->Fill(x,y);
+    histC->Fill(x,y);
+    histD->Fill(x,y);
+    
+  }//end loop tree
+  
+  C_ABCD->cd(3);
+  histA->Draw("COLZ");
+  C_ABCD->cd(1);
+  histB->Draw("COLZ");
+  C_ABCD->cd(2);
+  histC->Draw("COLZ");
+  C_ABCD->cd(4);
+  histD->Draw("COLZ");
+
   float gr1x[fitNum];
   float gr1y[fitNum];
   for(int i =0; i<fitNum; i++){
-    gr1x[i] = histf->GetBinCenter(i+1);
-    gr1y[i] = histf->GetBinContent(i+1,3)/histf->GetBinContent(i+1,1);
+    gr1x[i] = histA->GetBinCenter(i+1);
+    gr1y[i] = histB->GetBinContent(i+1,1)/histA->GetBinContent(i+1,1);
     cout << "ratio: (" << gr1x[i] << ", " << gr1y[i] << ")" << endl;
   }
   TGraph * gr1 = new TGraph(fitNum, gr1x, gr1y);
-  // gr1->Draw("A*");
+  //gr1->Draw("A*");
 
   TF1 *flin1 = new TF1("flin1", "[0]+[1]*x",borderv1a, borderv1b );
   gr1->Fit("flin1","R");
   Double_t par[2];
   flin1->GetParameters(par);
   cout << "Fit result: ratio = " << par[0] << " + " << par[1] << "*x" << endl;
-
-  //for(int i = 
-  cout << "bin = " << int(borderv2a/(f_xWidth)) << ", hist " << histf->GetBinCenter(int(borderv2a/(f_xWidth)))<<endl;
-
-
 
   float classicalEstimate = nD*nB/nA;
 
