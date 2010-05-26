bc7b2be158a411c166a5bcf6cd48564d5622f9d2
kreis <>
2010-05-26 15:56:55 +0000

    doesn't crash - now using flavour instead of genparton->pdgid. add jet multiplicity

--- PATtools/qcdABCD/TagJet/plugins/TagJet.cc
+++ PATtools/qcdABCD/TagJet/plugins/TagJet.cc
@@ -13,7 +13,7 @@
 //
 // Original Author:  Ben Kreis
 //         Created:  Tue May 25 22:38:50 CEST 2010
-// $Id$
+// $Id: TagJet.cc,v 1.1 2010/05/25 22:11:39 kreis Exp $
 //
 //
 
@@ -58,6 +58,9 @@ class TagJet : public edm::EDAnalyzer {
   edm::InputTag jetSrc_;
   
   TTree *tree1_;
+  float tree1_nAllJets_;
+  float tree1_nTightJets_;
+  float tree1_nLooseJets_;
   float tree1_eta_;
   float tree1_pt_;
   float tree1_btag_;
@@ -107,24 +110,67 @@ TagJet::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
   edm::Handle<edm::View<pat::Jet> > jets;
   iEvent.getByLabel(jetSrc_,jets);
 
+  int nAllJets = 0;
+  int nTightJets = 0;
+  int nLooseJets = 0;
+  //get jet multiplicity
   for(edm::View<pat::Jet>::const_iterator jet=jets->begin(); jet!=jets->end(); ++jet){
-    std::cout << "START" << std::endl;
-    std::cout << jet->genParton()->pdgId() << std::endl;
-    float jetid = jet->genParton()->pdgId();
-    std::cout << "CHECKED ID" << std::endl;
-    tree1_eta_ = jet->eta();
-    tree1_pt_ = jet->pt();
-    tree1_btag_ = 0.0;
-    tree1_bjet_ = 0.0;
-    std::cout << jetid << std::endl;
+    float jet_n90Hits = jet->jetID().n90Hits;
+    float jet_fHPD = jet->jetID().fHPD;
+    float jet_emf = jet->emEnergyFraction();
     
-    if(jet->bDiscriminator("simpleSecondaryVertexBJetTags")>=1.74) tree1_btag_ = 1.0;
-    if(fabs(jetid)==5.0) tree1_bjet_ = 1.0;
+    bool etaEMFcheck = true;
+    if( fabs(jet->eta())<2.6 && jet_emf <= 0.01) etaEMFcheck=false; 
     
-    std::cout << "CHECK" << std::endl;
+    //good jet check
+    if( jet_n90Hits >1.0 && jet_fHPD<0.98 && etaEMFcheck){
+      nAllJets++;
+      
+      //loose pT and eta cut
+      if( jet->pt()>30.0 && fabs(jet->eta())<5.0){
+	nLooseJets++;
+	
+	//tight pT and eta cut
+	if( jet->pt()>50.0 && fabs(jet->eta())<2.5){
+	  nTightJets++;
+	}//end tight jet
+	
+      }//end loose jet
 
-    tree1_->Fill();
+    }//good jet check
+  }//jet loop
 
+
+  for(edm::View<pat::Jet>::const_iterator jet=jets->begin(); jet!=jets->end(); ++jet){
+   
+    float jet_n90Hits = jet->jetID().n90Hits;
+    float jet_fHPD = jet->jetID().fHPD;
+    float jet_emf = jet->emEnergyFraction();
+    
+    bool etaEMFcheck = true;
+    if( fabs(jet->eta())<2.6 && jet_emf <= 0.01) etaEMFcheck=false; 
+    
+    //good jet check
+    if( jet_n90Hits >1.0 && jet_fHPD<0.98 && etaEMFcheck){
+      
+
+      float jetid = 0;
+      //std::cout << "pf: " << jet->partonFlavour() << ", pdg: " <<  jet->genParton()->pdgId();
+      jetid = jet->partonFlavour();
+      
+      tree1_nAllJets_ = nAllJets;
+      tree1_nTightJets_ = nTightJets;
+      tree1_nLooseJets_ = nLooseJets;
+      tree1_eta_ = jet->eta();
+      tree1_pt_ = jet->pt();
+      tree1_btag_ = 0.0;
+      tree1_bjet_ = 0.0;
+      if(jet->bDiscriminator("simpleSecondaryVertexBJetTags")>=1.74) tree1_btag_ = 1.0;
+      if(fabs(jetid)==5.0) tree1_bjet_ = 1.0;
+      
+      tree1_->Fill();
+    
+    }//good jet check
   }//end jet loop
 
 }
@@ -137,6 +183,9 @@ TagJet::beginJob()
 
   edm::Service<TFileService> fs;
   tree1_ = fs->make<TTree>("T_JetTag", "Tagging Efficiency Tree");
+  tree1_->Branch("nAllJets", &tree1_nAllJets_, "tree1_nAllJets_/F");
+  tree1_->Branch("nTightJets", &tree1_nTightJets_, "tree1_nTightJets_/F");
+  tree1_->Branch("nLooseJets", &tree1_nLooseJets_, "tree1_nLooseJets_/F");
   tree1_->Branch("eta", &tree1_eta_, "tree1_eta_/F");
   tree1_->Branch("pt", &tree1_pt_, "tree1_pt_/F");
   tree1_->Branch("btag", &tree1_btag_, "tree1_btag_/F");
