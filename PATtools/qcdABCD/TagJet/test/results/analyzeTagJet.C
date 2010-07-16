352d61f0a345840aaaac7a9bc9cabab4b91db6ae
kreis <>
2010-07-16 09:10:18 +0000

    add 1d plots for 3 eta regions

--- PATtools/qcdABCD/TagJet/test/results/analyzeTagJet.C
+++ PATtools/qcdABCD/TagJet/test/results/analyzeTagJet.C
@@ -10,6 +10,7 @@
 #include "TH1D.h"
 #include "TFile.h"
 #include "TCanvas.h"
+#include "TLegend.h"
 
 #include "analyzeTagJetInput.h"
 
@@ -44,7 +45,13 @@ void analyzeTagJet(){
 
   int nptBins = 21; 
   double ptBinArray[21] = {0.,30.,50.,100.,200.,300.,400.,500.,600.,700.,800.,900.,1000., 1200.,1400.,1600.,1800.,2000.,2300.,2600.,2900.};
-  
+
+  int nptBins1d = 25;
+  //  double ptBinArray1d[25] = {30, 33, 36, 39, 42, 45, 48, 51, 54, 57, 60, 63, 66, 69, 72, 75, 78, 81, 84, 87, 90, 93, 96, 99, 102};
+  double ptBinArray1d[25] = {30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 105, 110, 115, 120, 125, 130, 135, 140, 145, 150}; //max .1
+  // double ptBinArray1d[25] = {30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250, 260, 270};
+  // double ptBinArray1d[25] = {30, 50, 70, 90, 110, 130, 150, 170, 190, 210, 230, 250, 270, 290, 310, 330, 350, 370, 390, 410, 430, 450, 470, 490, 510}; //max .13
+
   TH2D* Htag = new TH2D("Htag", "H tag", nptBins-1, ptBinArray, 10, -5.0, 5.0);
   TH2D* Hjet = new TH2D("Hjet", "H jet", nptBins-1, ptBinArray, 10, -5.0, 5.0);
   TH2D* Hall = new TH2D("Hall", "H all", nptBins-1, ptBinArray, 10, -5.0, 5.0);
@@ -52,6 +59,19 @@ void analyzeTagJet(){
   TH2D* Heff_j = new TH2D("Heff_j", "H eff_j", nptBins-1, ptBinArray, 10, -5.0, 5.0);
   TH2D* Heff_te = new TH2D("Heff_te", "H eff_te", nptBins-1, ptBinArray, 10, -5.0, 5.0);  
   TH2D* Heff_je = new TH2D("Heff_je", "H eff_je", nptBins-1, ptBinArray, 10, -5.0, 5.0);  
+  
+  TH1D* HtagLow = new TH1D("HtagLow", "Htag Low", nptBins1d-1, ptBinArray1d);
+  TH1D* HtagMid = new TH1D("HtagMid", "Htag Mid", nptBins1d-1, ptBinArray1d);
+  TH1D* HtagHigh = new TH1D("HtagHigh", "Htag High", nptBins1d-1, ptBinArray1d);
+  TH1D* HallLow = new TH1D("HallLow", "Hall Low", nptBins1d-1, ptBinArray1d);
+  TH1D* HallMid = new TH1D("HallMid", "Hall Mid", nptBins1d-1, ptBinArray1d);
+  TH1D* HallHigh = new TH1D("HallHigh", "Hall High", nptBins1d-1, ptBinArray1d);
+  TH1D* HeffLow = new TH1D("HeffLow", "Heff Low", nptBins1d-1, ptBinArray1d);
+  TH1D* HeffMid = new TH1D("HeffMid", "Heff Mid", nptBins1d-1, ptBinArray1d);
+  TH1D* HeffHigh = new TH1D("HeffHigh", "Heff High", nptBins1d-1, ptBinArray1d);
+  TH1D* Heff_eLow = new TH1D("Heff_eLow", "Heff_e Low", nptBins1d-1, ptBinArray1d);
+  TH1D* Heff_eMid = new TH1D("Heff_eMid", "Heff_e Mid", nptBins1d-1, ptBinArray1d);
+  TH1D* Heff_eHigh = new TH1D("Heff_eHigh", "Heff_e High", nptBins1d-1, ptBinArray1d);
   
   Heff_t->SetMinimum(0.0);
   Heff_t->SetMaximum(.4);
@@ -74,9 +94,13 @@ void analyzeTagJet(){
   Heff_te->GetYaxis()->SetTitle(ytitle);
   Heff_je->GetYaxis()->SetTitle(ytitle);
 
+  TCanvas* C_byEta = new TCanvas("C_byEta", "Canvas byEta", 640, 480);
   TCanvas* C_all = new TCanvas("C_all", "Canvas all", 2560, 960);
   C_all->Divide(4,2);
 
+  //////////////////
+  // LOOP TO FILL //
+  //////////////////
   double ptmax = 0.0;
   for(int i = 0; i<numEntries; i++){
     InputChain->GetEvent(i);
@@ -90,17 +114,46 @@ void analyzeTagJet(){
       Htag->Fill(pt, eta, btag);
       Hjet->Fill(pt, eta, bjet);
       Hall->Fill(pt, eta, 1.0);
+
+
+      //
+      if(fabs(eta)>=0. && fabs(eta)<0.7){ //LOW
+	HtagLow->Fill(pt,btag);
+	HallLow->Fill(pt,1.0);
+      }
+      else if(fabs(eta)>=0.7 && fabs(eta)<1.4){ //MID
+	HtagMid->Fill(pt,btag);
+	HallMid->Fill(pt,1.0);
+      }
+      else if(fabs(eta)>=1.4 && fabs(eta)<2.4){ // HIGH
+	HtagHigh->Fill(pt,btag);
+	HallHigh->Fill(pt,1.0);
+      }
     }
-   
+
     if(pt>ptmax){
       ptmax = pt;
     }
     
   }
-
   cout << "ptmax: " << ptmax << endl;;
 
-  //CALCULATE EFFICIENCY
+  double tagLastBinLow = HtagLow->GetBinContent(nptBins1d-1);
+  HtagLow->SetBinContent(nptBins1d-1, tagLastBinLow+HtagLow->GetBinContent(nptBins1d));
+  double tagLastBinMid = HtagMid->GetBinContent(nptBins1d-1);
+  HtagMid->SetBinContent(nptBins1d-1, tagLastBinMid+HtagMid->GetBinContent(nptBins1d));
+  double tagLastBinHigh = HtagHigh->GetBinContent(nptBins1d-1);
+  HtagHigh->SetBinContent(nptBins1d-1, tagLastBinHigh+HtagHigh->GetBinContent(nptBins1d));
+  double allLastBinLow = HallLow->GetBinContent(nptBins1d-1);
+  HallLow->SetBinContent(nptBins1d-1, allLastBinLow+HallLow->GetBinContent(nptBins1d));
+  double allLastBinMid = HallMid->GetBinContent(nptBins1d-1);
+  HallMid->SetBinContent(nptBins1d-1, allLastBinMid+HallMid->GetBinContent(nptBins1d));
+  double allLastBinHigh = HallHigh->GetBinContent(nptBins1d-1);
+  HallHigh->SetBinContent(nptBins1d-1, allLastBinHigh+HallHigh->GetBinContent(nptBins1d));
+
+  ////////////////////////
+  //CALCULATE EFFICIENCY//
+  ////////////////////////
   int ptBins = Htag->GetNbinsX();
   int etaBins = Htag->GetNbinsY();
   
@@ -139,6 +192,50 @@ void analyzeTagJet(){
     }
   }
   
+  int ptBins1d = HtagLow->GetNbinsX();
+  for(int i = 1; i <= ptBins1d; i++){
+    
+    //LOW
+    double effLow = -0.1;
+    double eff_eLow = -0.1;
+    if(HallLow->GetBinContent(i)>0){
+      double tag = HtagLow->GetBinContent(i);
+      double all = HallLow->GetBinContent(i);
+      effLow = tag/all;
+      eff_eLow =  sqrt( (sqrt(tag)/tag)*(sqrt(tag)/tag)+(sqrt(all)/all)*(sqrt(all)/all) )*effLow;
+    }
+    HeffLow->SetBinContent(i, effLow);
+    HeffLow->SetBinError(i,eff_eLow);
+    Heff_eLow->SetBinContent(i, eff_eLow);
+ 
+    //MID
+    double effMid = -0.1;
+    double eff_eMid = -0.1;
+    if(HallMid->GetBinContent(i)>0){
+      double tag = HtagMid->GetBinContent(i);
+      double all = HallMid->GetBinContent(i);
+      effMid = tag/all;
+      eff_eMid =  sqrt( (sqrt(tag)/tag)*(sqrt(tag)/tag)+(sqrt(all)/all)*(sqrt(all)/all) )*effMid;
+    }
+    HeffMid->SetBinContent(i, effMid);
+    HeffMid->SetBinError(i,eff_eMid);
+    Heff_eMid->SetBinContent(i, eff_eMid);
+
+    //HIGH
+    double effHigh = -0.1;
+    double eff_eHigh = -0.1;
+    if(HallHigh->GetBinContent(i)>0){
+      double tag = HtagHigh->GetBinContent(i);
+      double all = HallHigh->GetBinContent(i);
+      effHigh = tag/all;
+      eff_eHigh =  sqrt( (sqrt(tag)/tag)*(sqrt(tag)/tag)+(sqrt(all)/all)*(sqrt(all)/all) )*effHigh;
+    }
+    HeffHigh->SetBinContent(i, effHigh);
+    HeffHigh->SetBinError(i,eff_eHigh);
+    Heff_eHigh->SetBinContent(i, eff_eHigh);
+  }
+
+ 
   C_all->cd(1);
   gPad->SetRightMargin(.18);
   Htag->Draw("COLZ");
@@ -167,8 +264,33 @@ void analyzeTagJet(){
 
   //  C_all->Print("plots.pdf");
 
-  cout << "fillNum: " << fillNum << endl;
 
+  TLegend* leg = new TLegend(0.63, 0.15, 0.88, 0.3);
+  leg->AddEntry(HeffLow, "0.0 <= |eta| < 0.7", "l"); 
+  leg->AddEntry(HeffMid, "0.7 <= |eta| < 1.4", "l"); 
+  leg->AddEntry(HeffHigh, "1.4 <= |eta| < 2.4", "l"); 
+  leg->SetFillColor(0);
+  leg->SetLineColor(0);
+  
+  HeffLow->SetMaximum(0.1);
+  HeffLow->SetTitle("");
+  HeffLow->GetXaxis()->SetTitle("pT");
+  HeffLow->GetYaxis()->SetTitle("SSVM Tagging Rate");
+  HeffLow->GetYaxis()->SetTitleOffset(1.5);
+  HeffLow->SetLineColor(kRed);
+  HeffMid->SetLineColor(kGreen);
+  HeffHigh->SetLineColor(kBlue);
+  C_byEta->cd();
+  gPad->SetLeftMargin(0.12);
+  HeffLow->Draw();
+  HeffMid->Draw("SAME");
+  HeffHigh->Draw("SAME");
+  leg->Draw();
+  
+
+  cout << "fillNum: " << fillNum << endl;
+  
+  C_byEta->Write();
   C_all->Write();
   Hall->Write();
   Hjet->Write();
@@ -176,6 +298,14 @@ void analyzeTagJet(){
   Heff_j->Write();
   Heff_te->Write();
   Heff_je->Write();
+
+  HeffLow->Write();
+  Heff_eLow->Write();
+  HeffMid->Write();
+  Heff_eMid->Write();
+  HeffHigh->Write();
+  Heff_eHigh->Write();
+
   fout.Close();
 
   cout << "End analyzeTagJet" << endl;
