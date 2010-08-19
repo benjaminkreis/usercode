ee5516315b7f9e6ff149581902172c6a74838327
kreis <>
2010-08-19 14:29:44 +0000

    to run on Spring10 MG. add MET. some attention to double/int/float.

--- PATtools/qcdABCD/FillABCD/plugins/FillABCD.cc
+++ PATtools/qcdABCD/FillABCD/plugins/FillABCD.cc
@@ -13,13 +13,14 @@
 //
 // Original Author:  Ben Kreis
 //         Created:  Thu May 13 06:03:28 CEST 2010
-// $Id: FillABCD.cc,v 1.2 2010/05/13 04:47:10 kreis Exp $
+// $Id: FillABCD.cc,v 1.3 2010/08/19 11:17:48 kreis Exp $
 //
 //
 
 
 // system include files
 #include <memory>
+#include <stdlib.h>
 
 // root include files
 #include "TTree.h"
@@ -37,6 +38,7 @@
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
 #include "DataFormats/PatCandidates/interface/Jet.h"
+#include "DataFormats/PatCandidates/interface/MET.h"
 #include "SimDataFormats/GeneratorProducts/interface/GenEventInfoProduct.h"
 
 //
@@ -56,16 +58,23 @@ class FillABCD : public edm::EDAnalyzer {
 
       // ----------member data ---------------------------
   unsigned int nEvents_;
+  
+  //input tags
   edm::InputTag jetSrc_;
+  edm::InputTag metSrc_;
+  double MG_;
+
   TTree *tree1_;
-  float tree1_minDPhi_;
-  float tree1_MHT_;
-  float tree1_qScale_;
-  float tree1_PtHat_;
-  float tree1_btag_;
-  float tree1_bjet_;
-  float tree1_nTightJets_;
-  float tree1_nLooseJets_;
+  double tree1_minDPhi_;
+  double tree1_MHT_;
+  double tree1_MET_;
+  double tree1_qScale_;
+  double tree1_PtHat_;
+  int tree1_btag_;
+  int tree1_bjet_;
+  int tree1_nTightJets_;
+  int tree1_nLooseJets_;
+  double tree1_MG_;
 
 };
 
@@ -81,7 +90,9 @@ class FillABCD : public edm::EDAnalyzer {
 // constructors and destructor
 //
 FillABCD::FillABCD(const edm::ParameterSet& iConfig):
-  jetSrc_(iConfig.getUntrackedParameter<edm::InputTag>("jetSrc" ))
+  jetSrc_(iConfig.getUntrackedParameter<edm::InputTag>("jetSrc" )),
+  metSrc_(iConfig.getUntrackedParameter<edm::InputTag>("metSrc" )),
+  MG_(iConfig.getUntrackedParameter<double>("MG"))
 
 {
    //now do what ever initialization is needed
@@ -115,11 +126,15 @@ FillABCD::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
    edm::Handle<edm::View<pat::Jet> > jets;
    iEvent.getByLabel(jetSrc_,jets);
    
+   // get met collection
+   edm::Handle<edm::View<pat::MET> > mets;
+   iEvent.getByLabel(metSrc_,mets);
+
    edm::Handle<GenEventInfoProduct> genEvtInfo;
    bool hasGenEventInfo = iEvent.getByLabel("generator", genEvtInfo);
 
-   float pthat = -1.0;
-   float qScale = -1.0;
+   double pthat = -1.0;
+   double qScale = -1.0;
    if(hasGenEventInfo){ 
      pthat = ( genEvtInfo->hasBinningValues() ? (genEvtInfo->binningValues())[0] : 0.0);
      qScale = genEvtInfo->qScale();
@@ -128,40 +143,41 @@ FillABCD::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 
    //Jet Stuff///////////////////////////////////////////////
    //////////////////////////////////////////////////////////
-   float dPhiMin = -1.0; 
+   double dPhiMin = -1.0; 
 
    int nTightJets = 0;
    int nLooseJets = 0;
    int nbtag =0, nbjet =0;
   
-   float myHT = 0; //scalar sum 
-   float myMHTx=0, myMHTy=0;
+   double myHT = 0; //scalar sum 
+   double myMHTx=0, myMHTy=0;
   
-   float jetpt1=0, jetpt2=0, jetpt3=0;
+   double jetpt1=0, jetpt2=0, jetpt3=0;
    edm::View<pat::Jet>::const_iterator jet1=jets->end(), jet2=jets->end(),jet3=jets->end();
   
-   float jetTpt1=0, jetTpt2=0, jetTpt3=0;
+   double jetTpt1=0, jetTpt2=0, jetTpt3=0;
    edm::View<pat::Jet>::const_iterator jetT1=jets->end(), jetT2=jets->end(),jetT3=jets->end();
-  
+   
    for(edm::View<pat::Jet>::const_iterator jet=jets->begin(); jet!=jets->end(); ++jet){
     
-     float jet_n90Hits = jet->jetID().n90Hits;
-     float jet_fHPD = jet->jetID().fHPD;
+     int jet_n90Hits = jet->jetID().n90Hits;
+     double jet_fHPD = jet->jetID().fHPD;
      float jet_emf = jet->emEnergyFraction();
 
      bool etaEMFcheck = true;
      if( fabs(jet->eta())<2.6 && jet_emf <= 0.01) etaEMFcheck=false; 
     
      //good jet check
-     if( jet_n90Hits >1.0 && jet_fHPD<0.98 && etaEMFcheck){
+     if( jet_n90Hits >1 && jet_fHPD<0.98 && etaEMFcheck){
        
        //check b
-       float jetid = 0;
+       int jetid = 0;
        jetid = jet->partonFlavour();
        if(fabs(jet->eta())<2.4 && jet->pt()>30.0){
 	 if(jet->bDiscriminator("simpleSecondaryVertexBJetTags")>=1.74) nbtag++;
-	 if(fabs(jetid)==5.0) nbjet++;
+	 if(abs(jetid)==5) nbjet++;
        }
+       
 
        //loose pT and eta cut
        if( jet->pt()>30.0 && fabs(jet->eta())<5.0){
@@ -223,19 +239,22 @@ FillABCD::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 	 }// end loose pT and eta cut
        }// end loose pT and eta cut
      }// end good jet cut
+    
    }// end jets loop
-  
-   float myMHT = sqrt(myMHTx*myMHTx+myMHTy*myMHTy);
-
-   //ABCD jet-phi stuff
-   float jet1phi = jetT1->phi();
-   float jet2phi = jetT2->phi();
-   float jet3phi = jetT3->phi();
-   float myMHTphi = atan2(myMHTy,myMHTx);
    
-   float jet1dPhi = acos(cos(jet1phi-myMHTphi));
-   float jet2dPhi = acos(cos(jet2phi-myMHTphi));
-   float jet3dPhi = acos(cos(jet3phi-myMHTphi));
+   double myMHT = sqrt(myMHTx*myMHTx+myMHTy*myMHTy);
+   
+   double myMET = mets->front().et();
+  
+   //ABCD jet-phi stuff
+   double jet1phi = jetT1->phi();
+   double jet2phi = jetT2->phi();
+   double jet3phi = jetT3->phi();
+   double myMHTphi = atan2(myMHTy,myMHTx);
+   
+   double jet1dPhi = acos(cos(jet1phi-myMHTphi));
+   double jet2dPhi = acos(cos(jet2phi-myMHTphi));
+   double jet3dPhi = acos(cos(jet3phi-myMHTphi));
    
    //mins
    if(jet1dPhi <= jet2dPhi && jet1dPhi <= jet3dPhi){ dPhiMin = jet1dPhi; }
@@ -243,15 +262,19 @@ FillABCD::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
    else if(jet3dPhi <= jet1dPhi && jet3dPhi <= jet2dPhi){ dPhiMin = jet3dPhi; }
    else{ assert(false); }
    
+   
+
    tree1_minDPhi_ = dPhiMin;
    tree1_MHT_= myMHT;
+   tree1_MET_ = myMET;
    tree1_qScale_ = qScale;
    tree1_PtHat_ = pthat;
+   tree1_MG_ = MG_; 
    tree1_btag_ = nbtag;
    tree1_bjet_ = nbjet;
    tree1_nTightJets_ = nTightJets;
    tree1_nLooseJets_ = nLooseJets;
-   tree1_->Fill();
+   tree1_->Fill(); 
   
    //end Jet Stuff//////////////////////////////////////////  
 }
@@ -264,15 +287,16 @@ FillABCD::beginJob()
 
   edm::Service<TFileService> fs;
   tree1_ = fs->make<TTree>("T_ABCD", "ABCD TREE");
-  tree1_->Branch("minDPhi", &tree1_minDPhi_, "tree1_minDPhi_/F");
-  tree1_->Branch("MHT", &tree1_MHT_, "tree1_MHT_/F");
-  tree1_->Branch("qScale", &tree1_qScale_, "tree1_qScale_/F");
-  tree1_->Branch("PtHat", &tree1_PtHat_, "tree1_PtHat_/F");
-  tree1_->Branch("btag", &tree1_btag_, "tree1_btag_/F");
-  tree1_->Branch("bjet", &tree1_bjet_, "tree1_bjet_/F");
-  tree1_->Branch("nTightJets", &tree1_nTightJets_, "tree1_nTightJets_/F");
-  tree1_->Branch("nLooseJets", &tree1_nLooseJets_, "tree1_nLooseJets_/F");
-
+  tree1_->Branch("minDPhi", &tree1_minDPhi_, "tree1_minDPhi_/D");
+  tree1_->Branch("MHT", &tree1_MHT_, "tree1_MHT_/D");
+  tree1_->Branch("MET", &tree1_MET_, "tree1_MET_/D");
+  tree1_->Branch("qScale", &tree1_qScale_, "tree1_qScale_/D");
+  tree1_->Branch("PtHat", &tree1_PtHat_, "tree1_PtHat_/D");
+  tree1_->Branch("btag", &tree1_btag_, "tree1_btag_/I");
+  tree1_->Branch("bjet", &tree1_bjet_, "tree1_bjet_/I");
+  tree1_->Branch("nTightJets", &tree1_nTightJets_, "tree1_nTightJets_/I");
+  tree1_->Branch("nLooseJets", &tree1_nLooseJets_, "tree1_nLooseJets_/I");
+  tree1_->Branch("MG", &tree1_MG_, "tree1_MG_/D");
 
   nEvents_ = 0;
 
