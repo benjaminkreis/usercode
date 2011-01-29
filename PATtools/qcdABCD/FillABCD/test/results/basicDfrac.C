f2687f28eb7d096c52acb9199ede85a8935b8ffc
kreis <>
2011-01-29 23:05:34 +0000

    subtract SM

--- PATtools/qcdABCD/FillABCD/test/results/basicDfrac.C
+++ PATtools/qcdABCD/FillABCD/test/results/basicDfrac.C
@@ -7,9 +7,14 @@
 
 using namespace std;
 
+//remember to set subtractSM, SMfactor
+
 void doBasicDfrac(int nbcut = 0, TString cutType = ""){
+  bool subtractSM = true;
+  double SMfactor = 1.0;
+
   cout << "nbcut: " << cutType << nbcut << endl;
-  double pi=4*atan(1.0);
+  double pi=4*atan(1.0)+0.0001;
 
   TH1D* hDpass = new TH1D("hDpass", "hDpass", 1, 0.0, .3);
   TH1D* hDall  = new TH1D("hDall",  "hDall",  1, 0.0, .3);
@@ -25,15 +30,28 @@ void doBasicDfrac(int nbcut = 0, TString cutType = ""){
   hCeff->Sumw2();
 
   TChain* InputChain = FormChainJosh("pfpf");
+  TChain* InputChainSM = 0;
+  if(subtractSM) InputChainSM = FormChainJoshSM("pfpf");
   int numEntries = InputChain->GetEntries();
+  int numEntriesSM = 0;
+  if(subtractSM) numEntriesSM = InputChainSM->GetEntries();
   cout <<"numEntries: " << numEntries << endl;
+  cout <<"numEntriesSM: " << numEntriesSM << endl;
   
   double x, y, weight;
+  double xSM, ySM, weightSM;
   int nbtags;
+  int nbtagsSM;
   InputChain->SetBranchAddress("MET",&x);
   InputChain->SetBranchAddress("minDeltaPhiMET",&y);
   InputChain->SetBranchAddress("weight",&weight);
   InputChain->SetBranchAddress("nbSSVM",&nbtags);
+  if(subtractSM){
+    InputChainSM->SetBranchAddress("MET",&xSM);
+    InputChainSM->SetBranchAddress("minDeltaPhiMET",&ySM);
+    InputChainSM->SetBranchAddress("weight",&weightSM);
+    InputChainSM->SetBranchAddress("nbSSVM",&nbtagsSM);
+  }
 
   for(int i = 0; i<numEntries; i++){
     InputChain->GetEvent(i);
@@ -55,12 +73,38 @@ void doBasicDfrac(int nbcut = 0, TString cutType = ""){
       if(pass) hCpass->Fill(y, weight);
       if(pass) hDpass->Fill(y, weight);
     }//end MET check
-    
   }//end loop over InputChain
+
+
+  if(subtractSM){
+    for(int i = 0; i<numEntriesSM; i++){
+      InputChainSM->GetEvent(i);
+
+      int pass = 0;
+      if(cutType=="e"){
+	if(nbtagsSM==nbcut)pass =1;
+      }
+      else if(cutType=="ge"){
+	if(nbtagsSM>=nbcut)pass =1;
+      }
+      else{
+	cout << "INVALID CUT TYPE!" << endl;
+      }
+      
+      if(xSM>=150.0){
+	hCall->Fill(ySM, -SMfactor*weightSM);
+	hDall->Fill(ySM, -SMfactor*weightSM);
+	if(pass) hCpass->Fill(ySM, -SMfactor*weightSM);
+	if(pass) hDpass->Fill(ySM, -SMfactor*weightSM);
+      }//end MET check                         
+
+    }//end loop over InputChainSM
+  }//end if subtractSM
+
   
   hCeff->Divide(hCpass, hCall, 1., 1., "B");
   hDeff->Divide(hDpass, hDall, 1., 1., "B");
-
+  
   double Dfrac = hDeff->GetBinContent(1);
   double Dfrac_e = hDeff->GetBinError(1);
   double Cfrac = hCeff->GetBinContent(1);
